c1f309e5187f8e5133b48302558c7618f9159859
TDne <117970531+TDne@users.noreply.github.com>
2022-11-11 13:48:03 +0700

    ok

---
+++ app.js
@@ -0,0 +1,67 @@
+const form = document.querySelector('#create-account-form');
+const usernameInput = document.querySelector('#username');
+const titleInput = document.querySelector('#title');
+
+form.addEventListener('submit', (event)=>{
+    
+    validateForm();
+    console.log(isFormValid());
+    if(isFormValid()==true){
+        form.submit();
+     }else {
+         event.preventDefault();
+     }
+
+});
+
+function isFormValid(){
+    const inputContainers = form.querySelectorAll('.input-group');
+    let result = true;
+    inputContainers.forEach((container)=>{
+        if(container.classList.contains('error')){
+            result = false;
+        }
+    });
+    return result;
+} 
+
+function validateForm() {
+    //USERNAME
+    if(usernameInput.value.trim()==''){
+        setError(usernameInput, 'Điền đầy đủ họ và tên vào cái :))');
+    }else if(usernameInput.value.trim()=='Nguyễn Phương Uyên' || usernameInput.value.trim()=='nguyễn phương uyên'){
+        setSuccess(usernameInput);
+    }else {
+        setError(usernameInput, 'Điền chuẩn đi làm ơnnnn');
+    }
+    //TITLE
+    if(titleInput.value.trim()==''){
+        setError(titleInput, 'Phải điền cả cái này nữa :v');
+    }else {
+        setSuccess(titleInput);
+    }
+
+}
+
+function setError(element, errorMessage) {
+    const parent = element.parentElement;
+    if(parent.classList.contains('success')){
+        parent.classList.remove('success');
+    }
+    parent.classList.add('error');
+    const paragraph = parent.querySelector('p');
+    paragraph.textContent = errorMessage;
+}
+
+function setSuccess(element){
+    const parent = element.parentElement;
+    if(parent.classList.contains('error')){
+        parent.classList.remove('error');
+    }
+    parent.classList.add('success');
+}
+
+
+
+
+
